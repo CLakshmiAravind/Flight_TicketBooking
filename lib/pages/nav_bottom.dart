481700971417601a536fecf51814bb4eb1ecbe3a
CLakshmiAravind <n170848@rguktn.ac.in>
2023-01-22 19:18:10 +0530

    creation of navbar

--- lib/pages/nav_bottom.dart
+++ lib/pages/nav_bottom.dart
@@ -8,10 +8,44 @@ class NavbarBottom extends StatefulWidget {
 }
 
 class _NavbarBottomState extends State<NavbarBottom> {
+  int _selectedIndex = 1;
+  static final List _widgetOptions = [
+    Text('home'),
+    Text('Search'),
+    Text('Tickets'),
+    Text('Profile'),
+    Text('extra'),
+  ];
+
+  void _changeIndex(int index){
+    setState(() {
+    _selectedIndex = index;
+    });
+    print('${index}');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('lets start'),
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Book Tickets'),
+      centerTitle:true,
+    ),
+    body: Center(child: _widgetOptions[_selectedIndex]),
+    bottomNavigationBar: BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,                  // otherwise icons will move slightly when clicked
+      showUnselectedLabels: false,                          // label text will be displayed if it is true            
+      showSelectedLabels: false,                            // label text will be displayed if it is true
+      selectedItemColor: Color.fromARGB(255, 17, 92, 190),// selected icon will change its color
+      unselectedItemColor: Colors.black,                  // unselected icon will change its color
+      onTap: _changeIndex,                                  // takes function
+      currentIndex: _selectedIndex,                         // takes index of items list
+      items: const [
+      BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home',),
+      BottomNavigationBarItem(icon: Icon(Icons.search),label: 'Search'),
+      BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket_outlined),label: 'Ticket'),
+      BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
+    ]),
     );
   }
 }
